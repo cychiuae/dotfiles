c851991fea1d2b6c24071ef53ae16b1e22b27212
YinYin Chiu <cychiuae@connect.ust.hk>
2022-12-13 14:02:09 +0800

    Setup nvim tree

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,4 +1,15 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  open_on_setup = false,
+  open_on_setup_file = false,
+	view = {
+		mappings = {
+			list = {
+				{ key = "<C-n>", action = "create"}
+			},
+		},
+	},
+})
 
 vim.opt.termguicolors = true
 vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle <CR>', {noremap = true, silent = true})
+
