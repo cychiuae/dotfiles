1ad3a5b4d9921dccc0b351259ed766b93615b143
YinYin Chiu <cychiuae@connect.ust.hk>
2022-12-20 18:58:21 +0800

    Update home manager version

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -29,12 +29,13 @@ in
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "22.05";
+    stateVersion = "23.05";
   };
 
 
 
   home.packages = with pkgs; [
+    dua # disk usage util
     fd
     gcloudPkgs
     gnupg
